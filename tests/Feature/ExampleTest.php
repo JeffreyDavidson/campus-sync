85f8932917ece81a5cd6724b68868e85359cf584
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-06-10 14:11:11 -0400

    refactor: remove default tests

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use function Pest\Laravel\get;
-
-test('the application returns a successful response', function (): void {
-    $response = get('/');
-
-    $response->assertStatus(200);
-});
